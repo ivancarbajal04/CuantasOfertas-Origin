9a78181bdd647eabc049ad1137ad22ba66c40bc0
Aprende Programando <aprende.programando@bue.edu.ar>
2024-07-22 10:11:15 -0300

    Agregado de pestaña Corporativa similar a las demas, correccion de marcas gamer

--- pages/corporativo/index.js
+++ pages/corporativo/index.js
@@ -1,49 +1,54 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var form = document.getElementById('newsletterForm');
+function openCalendly() {
+    window.open('https://calendly.com/ivan-carabajal-gr_2', 'calendly', 'width=800,height=600');
+  }
 
-    form.addEventListener('submit', function(event) {
-        event.preventDefault(); // Evita que el formulario se envíe
-        
-        // Obtiene los valores de los campos del formulario
-        var name = document.getElementById('name').value;
-        var lastname = document.getElementById('lastname').value;
-        var nameCorp = document.getElementById('nameCorp').value;
-        var email = document.getElementById('email').value;
-        var phone = document.getElementById('phone').value;
-        var query = document.getElementById('query').value;
-        
-        // Crea un objeto con los datos capturados
-        var formData = {
-            "name": name,
-            "lastname": lastname,
-            "nameCorp": nameCorp,
-            "email": email,
-            "phone": phone,
-            "query": query
-        };
-        
-        // Convertir el objeto a formato JSON
-        var jsonData = JSON.stringify(formData);
-        
+function openContacto(){
+    window.open('../../pages/contacto/index.html','Contactenme', 'width=700,height=500' )
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Seleccionar elementos del modal
+    var modal = document.getElementById("myModal");
+    var btn = document.getElementById("openModalLink");
+    var span = document.getElementsByClassName("close")[0];
+    var form = document.getElementById("newsletterForm");
+
+    // Abrir el modal cuando se hace clic en el botón
+    btn.addEventListener("click", function() {
+        modal.style.display = "block";
+    });
+
+    // Cerrar el modal cuando se hace clic en la X
+    span.addEventListener("click", function() {
+        modal.style.display = "none";
+    });
+
+    // Cerrar el modal cuando se hace clic fuera del modal
+    window.addEventListener("click", function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    });
+
+    // Enviar el formulario del newsletter
+    form.addEventListener("submit", function(event) {
+        event.preventDefault(); // Evitar la acción predeterminada de enviar el formulario
+
+        var name = document.getElementById("name").value;
+        var email = document.getElementById("email").value;
+
+        // Datos a enviar
+        var formData = new FormData();
+        formData.append('name', name);
+        formData.append('email', email);
+
         // Configurar y enviar la solicitud AJAX
-        fetch('https://hooks.zapier.com/hooks/catch/18644409/37pbz7k/', {
-            method: 'POST',
-            headers: {
-                // 'Content-Type': 'application/json'
-            },
-            body: jsonData
-        })
-        .then(response => {
-            if (response.ok) {
-                console.log('Los datos fueron enviados correctamente a Zapier');
-                alert("Gracias por dejar tus datos, nos estaremos contactando a la brevedad")
-                location.href = "../../index.html"
-            } else {
-                console.error('Error al enviar los datos a Zapier');
-            }
-        })
-        .catch(error => {
-            console.error('Error al enviar los datos a Zapier:', error);
-        });
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', 'https://hooks.zapier.com/hooks/catch/18644409/3njj078/');
+        xhr.onload = function() {
+            modal.style.display = "none"; // Cerrar el modal
+            alert("¡Gracias por suscribirte, " + name + "!");
+        };
+        xhr.send(formData);
     });
 });
